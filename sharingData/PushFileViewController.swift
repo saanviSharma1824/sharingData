c68cf460237e3caedf5b6804fe1fa64a4db2e27f
emitech990 <emitech990@emitech990s.local>
2019-03-05 16:08:43 +0530

    changes in push file

--- sharingData/PushFileViewController.swift
+++ sharingData/PushFileViewController.swift
@@ -12,7 +12,7 @@ class PushFileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("FIles Created")
         // Do any additional setup after loading the view.
     }
     

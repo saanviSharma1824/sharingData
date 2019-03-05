96bc88b5ca0fc64ae40aa631c9d3977bb6d38cfd
emitech990 <emitech990@emitech990s.local>
2019-03-05 15:50:18 +0530

    A file Added

---
+++ sharingData/AViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AViewController.swift
+//  sharingData
+//
+//  Created by emitech990 on 05/03/19.
+//  Copyright © 2019 Imminent Software. All rights reserved.
+//
+
+import UIKit
+
+class AViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
